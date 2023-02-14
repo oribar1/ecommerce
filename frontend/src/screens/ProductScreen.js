91e2ffe8ac4d1f18638ecb4ed63c3385d04f92d1
oribar1 <oribar9243@gmail.com>
2023-02-14 15:59:28 +0200

    models

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,13 +1,24 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import { Link, useParams } from 'react-router-dom'
 import {Row,Col,Image,ListGroup,Card,Button} from 'react-bootstrap'
 import Rating from '../components/Rating'
-import products from '../products'
+import axios from 'axios'
 
-const ProductScreen = ({match}) => {
-  
-    const { id } = useParams();
-    const product= products.find((p)=>p._id===id)
+const ProductScreen = () => {
+    
+    const params= useParams();
+    const [product,setProduct]= useState({})
+    useEffect(()=>{
+        const fetchProduct= async ()=> {
+          const {data}= await axios.get(`/api/products/${params.id}`)
+          setProduct(data)
+        }
+    
+        fetchProduct()
+      },[params])
+
+    
+    
     return (
     <>
       <Link className='btn btn-dark my-3' to ='/'>
